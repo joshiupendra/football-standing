03db8fcd5b052a88acc8282d9a1e589c3108126d
Upendra Joshi <joshiupendra9@gmail.com>
2024-09-19 19:17:17 +0530

    implement football-standing-api

---
+++ football-standing-api/src/main/java/com/football/upendra/UpendraApplication.java
@@ -0,0 +1,13 @@
+package com.football.upendra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpendraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpendraApplication.class, args);
+	}
+
+}
---
+++ football-standing-api/src/test/java/com/football/upendra/UpendraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.football.upendra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpendraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
